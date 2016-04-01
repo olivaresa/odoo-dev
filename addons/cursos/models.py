92eb693dd1667d5fbf2da25ae12c429a79f64781
Alexander Olivares <olivaresa@gmail.com>
2016-04-01 15:30:03 +0000

    multiple Many2one relations

--- addons/cursos/models.py
+++ addons/cursos/models.py
@@ -6,7 +6,6 @@ class Cursos(models.Model):
     _name = 'cursos.curso'
 
     name = fields.Char(string="Nombre del curso", required=True)
-    instructor = fields.Char(string="Instructor", required=True)
     description = fields.Text(string="Descripción del Curso")
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsable", index=True)
 
@@ -16,6 +15,9 @@ class Seccion(models.Model):
 
     name = fields.Char(required=True, string="Nombre")
     start_date = fields.Date(string="Fecha de início")
-    duration = fields.Float(digits=(6, 2), help="Duración en días")
+    duration = fields.Float(digits=(6, 2), help="Duración en días", string="Duración")
     seats = fields.Integer(string="Número de puestos")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('cursos.curso', ondelete='cascade', string="Curso", required=True)
 
