0e9ba5c1f08869c375d1ddebb88ee45bd72f4f02
Alexander Olivares <olivaresa@gmail.com>
2016-04-04 15:16:21 +0000

    Onchange

--- addons/cursos/models.py
+++ addons/cursos/models.py
@@ -19,7 +19,7 @@ class Seccion(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duración en días", string="Duración")
     seats = fields.Integer(string="Número de puestos")
     active = fields.Boolean(default=True)
-    
+
     instructor_id = fields.Many2one('res.partner', string="Instructor", 
                     domain=['|', ('instructor', '=', True),
                     ('category_id.name', 'ilike', "Profesor")]
@@ -39,3 +39,21 @@ class Seccion(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+            
