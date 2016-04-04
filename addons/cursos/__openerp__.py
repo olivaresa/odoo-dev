5d30222484ca0a5ec4af9a801fc323b8288c2d17
Alexander Olivares <olivaresa@gmail.com>
2016-04-04 14:17:44 +0000

    Models and Views inheritance

--- addons/cursos/__openerp__.py
+++ addons/cursos/__openerp__.py
@@ -26,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'templates.xml',
         'views/openacademy.xml',
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     #'demo': [
