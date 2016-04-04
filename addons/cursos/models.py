63fe441803762f1abb3c0398a6d530000ceabd30
Alexander Olivares <olivaresa@gmail.com>
2016-04-04 15:01:25 +0000

    Computed Dependencies

--- addons/cursos/models.py
+++ addons/cursos/models.py
@@ -28,3 +28,13 @@ class Seccion(models.Model):
 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
